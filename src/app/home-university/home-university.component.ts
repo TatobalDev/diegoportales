5650edec0d13b0bbaeba5d34e572674d8c79a74b
Cristobal <cmartinez@queplan.cl>
2022-12-26 18:59:31 -0300

    CHG Se cambian textos de equipo docente y testimonios

--- src/app/home-university/home-university.component.ts
+++ src/app/home-university/home-university.component.ts
@@ -9,9 +9,9 @@ import { ViewEncapsulation } from '@angular/core';
 })
 export class HomeUniversityComponent implements OnInit {
 	skills : any = {
-		title : "Discover our School",
-		sub_title : "Igniting the spark of genius in every child.",
-		content : "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p><ul class='list-check circle primary text-black'><li>You Will Never Thought That Knowing Education</li><li>Never Mess With Education And Here's The Reasons Why</li><li>This Is Why This Year Will Be The Year Of Education</li></ul>",
+		title : "Educación de calidad",
+		sub_title : "Encendiendo la chispa del genio en cada niño.",
+		content : "<p>Entregar a nuestros estudiantes educación de calidad, de forma íntegra, propiciando oportunidades de movilización intelectual y social promoviendo oportunidades de aprendizaje permanentes en un contexto inclusivo y equitativo.</p><ul class='list-check circle primary text-black'><li>Formación técnica profesional</li><li>Respeto y buenos valores</li><li>Docentes de primer nivel</li></ul>",
 		content_link:"about-1",
 		exp_content:"<h2 class='no-title'>+18</h2><h4 class='title'>años educando con amor</h4>",
 		images_data:{
